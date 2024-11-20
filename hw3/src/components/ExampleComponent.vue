832daaf4cb67a31b7164ea7d49c98aee726069ff
Mikk Vahaste <mikk.vahaste@gmail.com>
2024-11-20 15:12:38 +0000

    Added simple ExampleComponent.vue, removed HelloWorld.vue

---
+++ hw3/src/components/ExampleComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="example-component">
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: bold;
+  color: black;
+}
+</style>
+
+<script>
+export default {
+  name: "ExampleComponent",
+  props: {
+    text: String,
+  },
+};
+</script>

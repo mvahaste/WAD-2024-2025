9481d861e81398616ca0a560be7f87d778aea503
Mikk Vahaste <mikk.vahaste@gmail.com>
2024-11-23 17:27:00 +0200

    Removed Google Fonts Inter import

--- hw3/src/App.vue
+++ hw3/src/App.vue
@@ -19,10 +19,8 @@ export default {
 </script>
 
 <style>
-/* Roboto font */
-@import url("https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 
-/* Reset sizing */
 * {
   box-sizing: border-box;
   margin: 0;
@@ -35,7 +33,6 @@ body {
   background-color: #f5f5f5;
 }
 
-/* Font rendering, global margins */
 #app {
   font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;

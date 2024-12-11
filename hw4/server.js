5853de575fb91a2317c66724fc41289631cf94bc
mvahaste <mikk.vahaste@gmail.com>
2024-12-11 19:22:46 +0200

    Added API endpoints

--- hw4/server.js
+++ hw4/server.js
@@ -13,7 +13,91 @@ app.use(express.json());
 
 app.use((req, _, next) => {
   console.log(req.method + " " + req.url);
+
   next();
+});
+
+// GET /api/posts
+app.get("/api/posts", async (req, res) => {
+  try {
+    const posts = await pool.query("SELECT * FROM posts");
+
+    res.json(posts.rows);
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// GET /api/posts/:id
+app.get("/api/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const posts = await pool.query("SELECT * FROM posts WHERE id = $1", [id]);
+
+    res.json(posts.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// POST /api/posts
+app.post("/api/posts", async (req, res) => {
+  try {
+    const post = req.body;
+
+    const newpost = await pool.query(
+      "INSERT INTO posts (author, content) values ($1, $2)    RETURNING*",
+      [post.author, post.content]
+    );
+
+    res.json(newpost);
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// PUT /api/posts/:id
+app.put("/api/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const post = req.body;
+
+    const updatepost = await pool.query(
+      "UPDATE posts SET (title, body, urllink) = ($2, $3, $4) WHERE id = $1",
+      [id, post.content]
+    );
+
+    res.json(updatepost);
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// DELETE /api/posts/:id
+app.delete("/api/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletepost = await pool.query("DELETE FROM posts WHERE id = $1", [
+      id,
+    ]);
+
+    res.json(deletepost);
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 app.listen(port, () => {

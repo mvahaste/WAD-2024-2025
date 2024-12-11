ccb8c64a6c6e5cf5642f76401db9d1de0e35cad7
mvahaste <mikk.vahaste@gmail.com>
2024-12-11 15:06:07 +0200

    Added queries to create users and posts table if they don't exist

--- hw4/database.js
+++ hw4/database.js
@@ -1,10 +1,21 @@
 const Pool = require("pg").Pool;
+
 const pool = new Pool({
   user: "postgres",
-  password: "[add your password here]",
-  database: "[add the name of your database here]",
+  password: "admin",
+  database: "WAD",
   host: "localhost",
   port: "5432",
 });
+
+// Users table
+pool.query(
+  "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username TEXT, password TEXT)"
+);
+
+// Posts table
+pool.query(
+  "CREATE TABLE IF NOT EXISTS posts (id SERIAL PRIMARY KEY, author INT, content TEXT, likes INT, created TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
+);
 
 module.exports = pool;

c8613045b46626e0d4b51cfa1c76e98ef5440eae
mvahaste <mikk.vahaste@gmail.com>
2024-12-11 19:26:30 +0200

    Added more dependencies

--- hw4/server.js
+++ hw4/server.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const pool = require("./database");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors());
+// We need to include "credentials: true" to allow cookies to be represented
+// Also "credentials: 'include'" need to be added in Fetch API in the Vue.js App
+app.use(cors({ origin: "http://localhost:8080", credentials: true }));
 
-// The express.json() function is a built-in middleware function in Express.
-// It parses incoming requests with JSON payloads and is based on body-parser.
+// Parses incoming requests with JSON payloads and is based on body-parser.
 app.use(express.json());
+// Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
+app.use(cookieParser());
 
 app.use((req, _, next) => {
+  // Log requests for debugging
   console.log(req.method + " " + req.url);
 
   next();

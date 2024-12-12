134765e71b368e4e2a5c3e0d187f2a35d59e3ec1
mvahaste <mikk.vahaste@gmail.com>
2024-12-12 19:51:49 +0200

    Added auth endpoints, comments

--- hw4/server.js
+++ hw4/server.js
@@ -25,8 +25,121 @@ app.use((req, _, next) => {
 	next();
 });
 
+app.listen(port, () => {
+	console.log("Server is listening to port " + port);
+});
+
+const secret = "very-secure-secret";
+const maxAge = 60 * 60;
+
+const generateJWT = (id) => {
+	return jwt.sign({ id }, secret, { expiresIn: maxAge });
+};
+
+// GET /api/auth
+// Get whether the user is authenticated or not.
+app.get("/auth/authenticate", async (req, res) => {
+	const token = req.cookies.jwt;
+	let authenticated = false;
+
+	try {
+		if (token) {
+			jwt.verify(token, secret, (err) => {
+				if (err) {
+					console.log("INVALID TOKEN: " + err.message);
+
+					res.send({ authenticated: authenticated });
+				} else {
+					console.log("VALID TOKEN!");
+
+					authenticated = true;
+
+					res.send({ authenticated: authenticated });
+				}
+			});
+		} else {
+			console.log("NO TOKEN!");
+
+			res.send({ authenticated: authenticated }); // authenticated = false
+		}
+	} catch (err) {
+		console.error(err.message);
+
+		res.status(400).send(err.message);
+	}
+});
+
+// POST /api/auth/signup
+// Sign up a new user.
+app.post("/auth/signup", async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		const salt = await bcrypt.genSalt();
+
+		const bcryptPassword = await bcrypt.hash(password, salt);
+
+		const authUser = await pool.query(
+			// Save the user to the databse with the email and hashed password
+			"INSERT INTO users(email, password) values ($1, $2) RETURNING *",
+			[email, bcryptPassword],
+		);
+
+		console.log(authUser.rows[0].id);
+
+		const token = generateJWT(authUser.rows[0].id);
+
+		res.status(201).cookie("jwt", token, { maxAge: 6000000, httpOnly: true }).json({ user_id: authUser.rows[0].id })
+			.send;
+	} catch (err) {
+		console.error(err.message);
+
+		res.status(400).send(err.message);
+	}
+});
+
+// POST /api/auth/login
+// Log in an existing user.
+app.post("/auth/login", async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+
+		if (user.rows.length === 0) return res.status(401).json({ error: "User is not registered" });
+
+		/* 
+        To authenticate users, you will need to compare the password they provide with the one in the database. 
+        bcrypt.compare() accepts the plain text password and the hash that you stored, along with a callback function. 
+        That callback supplies an object containing any errors that occurred, and the overall result from the comparison. 
+        If the password matches the hash, the result is true.
+
+        bcrypt.compare method takes the first argument as a plain text and the second argument as a hash password. 
+        If both are equal then it returns true else returns false.
+        */
+
+		const validPassword = await bcrypt.compare(password, user.rows[0].password);
+
+		if (!validPassword) return res.status(401).json({ error: "Incorrect password" });
+
+		const token = generateJWT(user.rows[0].id);
+
+		res.status(201).cookie("jwt", token, { maxAge: 6000000, httpOnly: true }).json({ user_id: user.rows[0].id })
+			.send;
+	} catch (error) {
+		res.status(401).json({ error: error.message });
+	}
+});
+
+// GET /api/auth/logout
+// Log out the user.
+app.get("/auth/logout", (_req, res) => {
+	res.status(202).clearCookie("jwt").json({ Msg: "cookie cleared" }).send;
+});
+
 // GET /api/posts
-app.get("/api/posts", async (req, res) => {
+// Get all posts.
+app.get("/api/posts", async (_req, res) => {
 	try {
 		const posts = await pool.query("SELECT * FROM posts");
 
@@ -39,6 +152,7 @@ app.get("/api/posts", async (req, res) => {
 });
 
 // GET /api/posts/:id
+// Get a post by it's ID
 app.get("/api/posts/:id", async (req, res) => {
 	try {
 		const { id } = req.params;
@@ -54,6 +168,7 @@ app.get("/api/posts/:id", async (req, res) => {
 });
 
 // POST /api/posts
+// Create a new post.
 app.post("/api/posts", async (req, res) => {
 	try {
 		const post = req.body;
@@ -72,6 +187,7 @@ app.post("/api/posts", async (req, res) => {
 });
 
 // PUT /api/posts/:id
+// Update a post by it's ID
 app.put("/api/posts/:id", async (req, res) => {
 	try {
 		const { id } = req.params;
@@ -92,6 +208,7 @@ app.put("/api/posts/:id", async (req, res) => {
 });
 
 // DELETE /api/posts/:id
+// Delete a post by it's ID
 app.delete("/api/posts/:id", async (req, res) => {
 	try {
 		const { id } = req.params;
@@ -106,6 +223,16 @@ app.delete("/api/posts/:id", async (req, res) => {
 	}
 });
 
-app.listen(port, () => {
-	console.log("Server is listening to port " + port);
+// DELETE /nuke
+// Delete all posts.
+app.delete("/nuke", async (req, res) => {
+	try {
+		const deleteAllPosts = await pool.query("DELETE FROM posts");
+
+		res.json(deleteAllPosts);
+	} catch (err) {
+		console.error(err.message);
+
+		res.status(500).json({ message: "Internal server error" });
+	}
 });
